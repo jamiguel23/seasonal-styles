e220d7fa3367cdfc1f538d79d70d0619b3bdd866
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2023-01-24 10:55:47 -0800

    formatted

--- js/seasonal-styles.js
+++ js/seasonal-styles.js
@@ -1,11 +1,6 @@
 $(function(){
 
-  let $theDiv = $('#myDiv');
-
-  $theDiv
-  .css('background-color', 'yellow')
-  .css('border' , '1px solid red')
-  .append(' next text form jquery');
+  
 });
 
 // document.querySelector('div#myDiv').innerHTML= 'new text'
